000c707e5c61a4f180730d6cf15cd494e3f9740e
YURII <ymarchenko@i.ua>
2022-08-13 17:59:12 +0300

    corr

--- 04_HW/4_1/4_1.js
+++ 04_HW/4_1/4_1.js
@@ -3,15 +3,22 @@ console.log(arr);
 let num;
 
 while (true) {
-  const num = prompt("Вкажіть число");
-  arr.push(num);
-  console.log(`Додано ще один елемент масиву [${arr}]`);
+  const num = prompt();
   if (num == null) {
     break;
   }
+  arr.push(num);
+  console.log(`Додано ще один елемент масиву [${arr}]`);
 }
-arr.pop();
-console.log(`Корегування масиву(видалення null) [${arr}]`);
-console.log(`Відсоротований масив  [${arr.sort((a, b) => a - b)}]`);
-arr.sort((a, b) => a - b).splice(1, 3);
+
+console.log(
+  `Відсоротований масив  [${arr.sort(function (a, b) {
+    return a - b;
+  })}]`
+);
+arr
+  .sort(function (a, b) {
+    return a - b;
+  })
+  .splice(1, 3);
 console.log(`Масив без елементів з 2 по 4  [${arr}]`);
--- 04_HW/4_2/4_2.js
+++ 04_HW/4_2/4_2.js
@@ -1,27 +1,6 @@
 let arr = [
-  16,
-  -37,
-  54,
-  -4,
-  72,
-  -56,
-  47,
-  4,
-  -16,
-  25 - 37,
-  46,
-  4 - 51,
-  27,
-  -63,
-  4,
-  -54,
-  76,
-  -4,
-  12,
-  235,
-  4,
-  47,
-  5,
+  16, -37, 54, -4, 72, -56, 47, 4, -16, 25, -37, 46, 4, -51, 27, -63, 4, -54,
+  76, -4, 12, 235, 4, 47, 5,
 ];
 console.log(arr);
 
@@ -29,8 +8,8 @@ console.log(arr);
 const arr1 = arr.filter(function (element) {
   return element > 0;
 });
-console.log(arr1);
-console.log(arr1.length);
+// console.log(arr1);
+console.log(`Кількість позитивних елементів масиву  ${arr1.length}`);
 const sum = arr
   .filter(function (element) {
     return element > 0;
@@ -38,29 +17,31 @@ const sum = arr
   .reduce(function (prev, element) {
     return prev + element;
   }, 0);
-console.log(sum);
+console.log(`Сума позитивних елементів масиву ${sum}`);
 
 // 2.2
-console.log(Math.min(...arr));
-console.log(arr.indexOf(Math.min(...arr)));
+console.log(`Мінімальний елемент масиву  ${Math.min(...arr)}`);
+const indexMin = arr.indexOf(Math.min(...arr));
+console.log(`Порядковий номер мінімального елементу масиву  ${indexMin + 1}`);
 
 // 2.3
-console.log(Math.max(...arr));
-console.log(arr.indexOf(Math.max(...arr)));
+console.log(`Максимальний  елемент  масиву  ${Math.max(...arr)}`);
+const indexMax = arr.indexOf(Math.max(...arr));
+console.log(`Порядковий номер максимального елемента масиву  ${indexMax + 1}`);
 
 // 2.4
 const arr3 = arr.filter(function (element) {
   return element < 0;
 });
-console.log(arr3);
-console.log(arr3.length);
+// console.log(arr3);
+console.log(`Кількість негативних елементів масиву ${arr3.length}`);
 
 // 2.5
 const arr4 = arr.filter(function (element) {
   return element > 0 && element % 2 !== 0;
 });
-console.log(arr4);
-console.log(arr4.length);
+// console.log(arr4);
+console.log(`Кількість позитивних непарних елементів масиву ${arr4.length}`);
 
 // 2.6
 const sum2 = arr
@@ -71,7 +52,7 @@ const sum2 = arr
     return prev + element;
   }, 0);
 
-console.log(sum2);
+console.log(`Сума позитивних парних елементів масиву ${sum2}`);
 
 // 2.7
 const arr5 = arr.filter(function (element) {
@@ -80,8 +61,8 @@ const arr5 = arr.filter(function (element) {
 const pro = arr5.reduce(function (prev, element) {
   return prev * element;
 }, 1);
-console.log(arr5);
-console.log(pro);
+// console.log(arr5);
+console.log(`Множення позитивних  елементів масиву ${pro}`);
 
 // 2.8
 
